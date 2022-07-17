bd0db0d46b54144773aa4a68427417ab89e9598a
swayam <codingswayam24@gmail.com>
2022-07-17 09:33:39 +0530

    Navbar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+
+function App() {
+  return (
+    <Navbar 
+      navbar="This is an navbar"
+      home="This is a home"
+      link="this is an link"
+    />
+  )
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+
+
+const Navbar = ({navbar, home, link}) => {
+    return (
+        <div className="App">
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            {navbar}
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                 {home}
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  {link}
+                </a>
+              </li>
+            </ul>
+        </div>
+        </div>
+      </nav>
+    </div>
+    )
+}
+
+export default Navbar;
